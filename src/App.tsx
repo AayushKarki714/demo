1e35a2c527ecb3c9a01598a93136613ed27d875b
Aayush <karkiayush201@gmail.com>
2022-12-26 23:48:25 +0545

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/404";
+import Chat from "./pages/Chat";
+import CodingBuddy from "./pages/CodingBuddy";
+import Date from "./pages/Date";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main
+      className="h-screen 
+     bg-[#18191a] font-poppins"
+    >
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />}>
+          <Route path="date" element={<Date />} />
+          <Route path="date" element={<CodingBuddy />} />
+          <Route path="chat" element={<Chat />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   );
-}
+};
 
 export default App;
